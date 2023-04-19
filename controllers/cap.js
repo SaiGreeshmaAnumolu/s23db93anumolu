b28a8179a137b13ec6bc95fcd012ecfc52e578f1
SaiGreeshmaAnumolu <123037725+SaiGreeshmaAnumolu@users.noreply.github.com>
2023-04-19 17:55:03 -0500

    SS8 done

--- controllers/cap.js
+++ controllers/cap.js
@@ -128,3 +128,16 @@ exports.cap_create_Page = function(req, res) {
     res.send(`{'error': '${err}'}`);
     }
    };
+   // Handle building the view for updating a cap.
+// query provides the id
+exports.cap_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await cap.findById(req.query.id)
+    res.render('capupdate', { title: 'cap Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
