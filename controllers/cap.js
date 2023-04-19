d3cb8c3a5c7cdaf5a048cb545663e482913cbef1
SaiGreeshmaAnumolu <123037725+SaiGreeshmaAnumolu@users.noreply.github.com>
2023-04-19 17:41:46 -0500

    SS7 done

--- controllers/cap.js
+++ controllers/cap.js
@@ -115,3 +115,16 @@ exports.cap_view_one_Page = async function(req, res) {
     res.send(`{'error': '${err}'}`);
     }
    };
+   // Handle building the view for creating a cap.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.cap_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('capcreate', { title: 'cap Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
--- routes/cap.js
+++ routes/cap.js
@@ -8,3 +8,5 @@ router.get('/', cap_controlers.cap_view_all_Page );
 router.get('/detail', cap_controlers.cap_view_one_Page);
 
 module.exports = router;
+/* GET create cap page */
+router.get('/create', cap_controlers.cap_create_Page);
