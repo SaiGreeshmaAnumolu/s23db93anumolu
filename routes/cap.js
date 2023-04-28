692c4c2a6d0a1620cc2741ebf3c7b6b0b1b87b93
SaiGreeshmaAnumolu <123037725+SaiGreeshmaAnumolu@users.noreply.github.com>
2023-04-27 21:19:09 -0500

    ss6 done

--- routes/cap.js
+++ routes/cap.js
@@ -4,13 +4,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', cap_controlers.cap_view_all_Page );
+
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+}   
+/* GET create update page */
+router.get('/update',secured, cap_controlers.cap_update_Page);
 /* GET detail cap page */
-router.get('/detail', cap_controlers.cap_view_one_Page);
+router.get('/detail',cap_controlers.cap_view_one_Page);
+
+/* GET create cap page */
+router.get('/create',secured, cap_controlers.cap_create_Page);
+
+
+/* GET delete cap page */
+router.get('/delete',secured, cap_controlers.cap_delete_Page);
 
 module.exports = router;
-/* GET create cap page */
-router.get('/create', cap_controlers.cap_create_Page);
-/* GET create update page */
-router.get('/update', cap_controlers.cap_update_Page);
-/* GET delete cap page */
-router.get('/delete', cap_controlers.cap_delete_Page);
